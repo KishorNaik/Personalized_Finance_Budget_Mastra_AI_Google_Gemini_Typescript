cd8a64e8de0849ba9a38b66506dae08207db97c0
Kishor Naik <kishor.naik011.net@gmail.com>
2025-08-06 00:13:18 +0530

    - Add Pub/Sub User Shared Cache Services

---
+++ api/src/modules/users/shared/cache/events/publish/inde.ts
@@ -0,0 +1,72 @@
+import {
+	bullMqRedisConnection,
+	ExceptionsWrapper,
+	GuardWrapper,
+	IServiceHandlerVoidAsync,
+	JsonString,
+	RequestReplyProducerBullMq,
+	Result,
+	ResultError,
+	ResultFactory,
+	sealed,
+	SenderReceiverProducerBullMq,
+	SendReceiverMessageBullMq,
+	Service,
+	StatusEnum,
+	VOID_RESULT,
+	VoidResult,
+} from '@kishornaik/utils';
+import { randomUUID } from 'crypto';
+
+// Define Queue
+const queueName = 'user-shared-cache-event-queue';
+const producer = new SenderReceiverProducerBullMq(bullMqRedisConnection);
+producer.setQueues(queueName);
+
+export interface IPublishUserSharedCacheDomainEventServiceParameters {
+	identifier: string;
+	status: StatusEnum;
+	traceId: string;
+}
+
+export interface IPublishUserSharedCacheDomainEventService
+	extends IServiceHandlerVoidAsync<IPublishUserSharedCacheDomainEventServiceParameters> {}
+
+@sealed
+@Service()
+export class PublishUserSharedCacheDomainEventService implements IPublishUserSharedCacheDomainEventService{
+  public async handleAsync(params: IPublishUserSharedCacheDomainEventServiceParameters): Promise<Result<VoidResult, ResultError>> {
+    return await ExceptionsWrapper.tryCatchResultAsync(async ()=>{
+      const {identifier,status,traceId}=params;
+
+      // Guard
+      const guardResult=new GuardWrapper()
+      .check(identifier,'identifier')
+      .check(status,'status')
+      .check(traceId,'traceId')
+      .validate();
+      if(guardResult.isErr())
+        return ResultFactory.error(guardResult.error.statusCode,guardResult.error.message);
+
+      // Generate Payload
+      const payload={
+        identifier:identifier,
+        status:status
+      }
+
+      // Generate Message BullMq Payload
+      const message:SendReceiverMessageBullMq<JsonString>={
+        correlationId:randomUUID().toString(),
+        timestamp:new Date().toISOString(),
+        traceId:traceId,
+        data:JSON.stringify(payload) as JsonString
+      }
+
+      // Publish Event
+      await producer.sendAsync<JsonString>(`JOB:${queueName}`,message);
+
+      // Return
+      return ResultFactory.success(VOID_RESULT);
+    });
+  }
+}
---
+++ api/src/modules/users/shared/cache/events/subscribe/index.ts
@@ -0,0 +1,50 @@
+import { logger } from '@/shared/utils/helpers/loggers';
+import { getQueryRunner } from '@kishornaik/db';
+import {
+	bullMqRedisConnection,
+	Container,
+	JsonString,
+	SenderReceiverConsumerBullMq,
+	StatusEnum,
+	TransactionsWrapper,
+	WorkerBullMq,
+} from '@kishornaik/utils';
+import { UserSharedCacheService } from '../../set';
+
+
+const queueName = 'user-shared-cache-event-queue';
+const consumer = new SenderReceiverConsumerBullMq(bullMqRedisConnection);
+
+export const subscribeUserSharedCacheDomainEvent: WorkerBullMq = async () => {
+	logger.info(`User Shared Cache Subscribe Worker Starting...`);
+
+	const worker = await consumer.startConsumingAsync<JsonString>(queueName, async (message) => {
+		const { data, correlationId, timestamp, traceId } = message.data;
+		logger.info(
+			`User Shared Cache Event Job started: traceId: ${traceId} | correlationId: ${correlationId} | jobId: ${message.id}`
+		);
+
+		// Payload
+		const payload: { identifier: string; status: StatusEnum } = JSON.parse(data);
+
+    // Call Cache Service
+		const userSharedCacheService = Container.get(UserSharedCacheService);
+    await userSharedCacheService.handleAsync({
+      identifier:payload.identifier,
+      status:payload.status,
+    })
+
+	});
+
+	worker.on('completed', (job) => {
+		logger.info(
+			`User Shared Cache Event Completed: traceId: ${job.data.traceId} | correlationId: ${job.data.correlationId} | jobId: ${job.id}`
+		);
+	});
+
+	worker.on('failed', (job, err) => {
+		logger.error(
+			`User Shared Cache Event Failed: traceId: ${job.data.traceId} | correlationId: ${job.data.correlationId} | jobId: ${job.id} | error: ${err.message}`
+		);
+	});
+};
