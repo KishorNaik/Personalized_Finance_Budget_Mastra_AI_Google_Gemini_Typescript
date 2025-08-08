96c292f600725e15ccd30d020fd73fd567b4d966
Kishor Naik <kishor.naik011.net@gmail.com>
2025-08-08 18:39:00 +0530

    - Implement Get User By Id Request Dto - Implement Get User By Id Response Dto

---
+++ api/src/modules/users/apps/features/v1/getUserByIdentifier/contracts/index.ts
@@ -0,0 +1,26 @@
+import { IsSafeString, StatusEnum } from "@kishornaik/utils";
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+// #region Request Dto
+export class GetUserByIdentifierRequestDto {
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  @IsSafeString({ message: 'Name must not contain HTML or JavaScript code' })
+  @Type(() => String)
+  public identifier?: string;
+}
+// #endregion
+
+// #region Response Dto
+export class GetUserByIdentifierResponseDto {
+  public identifier?: string;
+  public firstName?: string;
+  public lastName?: string;
+  public email?: string;
+  public status?: StatusEnum;
+  public userName?: string;
+}
+// #endregion
