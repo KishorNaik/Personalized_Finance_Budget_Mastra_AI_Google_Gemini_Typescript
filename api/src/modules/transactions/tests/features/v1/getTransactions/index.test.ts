6ceba07d2f8bb1123eed59568386756738369b26
Kishor Naik <kishor.naik011.net@gmail.com>
2025-08-09 21:28:47 +0530

    - Get Transaction test has been done

---
+++ api/src/modules/transactions/tests/features/v1/getTransactions/index.test.ts
@@ -0,0 +1,67 @@
+import expect from 'expect';
+import { describe, it } from 'node:test';
+import { destroyDatabase, initializeDatabase, TransactionType } from '@kishornaik/db';
+import { mediator } from '@/shared/utils/helpers/medaitR';
+import { RequestData, RoleEnum, ValidateEnv } from '@kishornaik/utils';
+import { randomUUID } from 'node:crypto';
+import { App } from '@/app';
+import { restApiModulesFederation } from '@/modules/app.Module';
+import jwt from 'jsonwebtoken';
+import { SECRET_KEY } from '@/config/env';
+import request from 'supertest';
+import { GetTransactionsRequestDto } from '@/modules/transactions/apps/features/v1/getTransactions';
+
+process.env.NODE_ENV = 'development';
+ValidateEnv();
+
+const appInstance = new App();
+appInstance.initializeRestApiRoutes([...restApiModulesFederation]);
+appInstance.initializeErrorHandling();
+const app = appInstance.getServer();
+
+describe(`Get-Transaction-Unit-Test`, () => {
+	// node --trace-deprecation --test --test-name-pattern='should_return_200_when_all_services_execute_successfully' --require ts-node/register -r tsconfig-paths/register ./src/modules/transactions/tests/features/v1/getTransactions/index.test.ts
+	it(`should_return_200_when_all_services_execute_successfully`, async () => {
+		await initializeDatabase();
+
+		// UserId
+		const userId: string = `625b0ab1-77d0-4a3e-ae75-d0bf93cdc132`;
+
+		// Request
+		const requestDto: GetTransactionsRequestDto = new GetTransactionsRequestDto();
+		requestDto.month=8;
+    requestDto.year=2025;
+
+		// Endpoint
+		const endpoint = `/api/v1/transactions?month=${requestDto.month}&year=${requestDto.year}`;
+
+		// Jwt Id
+		const token = jwt.sign({ id: userId, role: RoleEnum.USER }, SECRET_KEY, {
+			expiresIn: '1h',
+			algorithm: 'HS256',
+		});
+
+		const response = await request(app)
+			.get(endpoint)
+			.set('authorization', `Bearer ${token}`);
+
+		if (response.status !== 200) {
+			console.log(`error: ${JSON.stringify(response.body)}`);
+			await destroyDatabase();
+			setTimeout(() => {
+				process.exit(0);
+			}, 5000);
+
+			expect(false).toBe(true);
+			return;
+		}
+
+		console.log(`Response: ${JSON.stringify(response.body)}`);
+
+		await destroyDatabase();
+		setTimeout(() => {
+			process.exit(0);
+		}, 5000);
+		expect(true).toBe(true);
+	});
+});
